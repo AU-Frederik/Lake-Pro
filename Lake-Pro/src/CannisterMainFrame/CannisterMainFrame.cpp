78802c700bd37745079b88ba096b1f1f5191bf9b
AU-Frederik <201911020@post.au.dk>
2024-11-07 14:10:48 +0100

    Added TSYS01 (outside temp) to CannisterMainFrame

--- Lake-Pro/src/CannisterMainFrame/CannisterMainFrame.cpp
+++ Lake-Pro/src/CannisterMainFrame/CannisterMainFrame.cpp
@@ -2,6 +2,18 @@
 // Author: Mechatronics Group 3
 // Date: 2024-10-22
 
+/*
+SYSTEM RIGHT NOW CONSISTS OF: 
+- DHT22 (HUMIDITY AND TEMP SENSOR)
+- GROVE SCD30 (CO2, HUMIDITY AND TEMP SENSOR)
+- GROVE BAROMETER (PRESSURE SENSOR INSIDE)
+- TSYS01 (TEMP SENSOR IN WATER)
+
+MISSING: 
+- FIGARO NGM2611-E13 (CH4 SENSOR) THROUGH ADS1115 (ADC)
+- BAR100 (OUTSIDE PRESSURE SENSOR)
+*/
+
 
 #include "SCD30.h"
 #include <HP20x_dev.h>
@@ -10,22 +22,24 @@
 #include <KalmanFilter.h>
 #include <DHT.h>
 #include <Adafruit_ADS1X15.h>
+#include <TSYS01.h>
 
 #define DHT22_PIN 12
 #define DHTTYPE DHT22
 
-String dataString = "";
+String dataString;
 
 /* Instance */
 KalmanFilter t_filter;    //temperature filter
 KalmanFilter p_filter;    //pressure filter
 DHT dht(DHT22_PIN, DHTTYPE);
 Adafruit_ADS1115 ads;
+TSYS01 outsideTempSensor;
 
 void SCD30_Measure();
 void HP20_Measure();
 void DHT22_Measure();
-
+void outsideTemp_Measure();
 
 float CO2_Measurement_SCD = 0;
 float humidity_Measurement_SCD = 0;
@@ -41,6 +55,8 @@ float average_Humidity = 0;
 
 float CH4_Measurement = 0;
 
+float outsideTemperature_Measurement;
+
 void setup() {
     // Setup I2C
     Wire.begin();
@@ -52,9 +68,14 @@ void setup() {
     // Setup sensors
     HP20x.begin();
     scd30.initialize();
+    while (!outsideTempSensor.init()) {
+        Serial.println("TSYS01 device failed to initialize!");
+        delay(2000);
+    }
 }
 
 void loop() {
+    dataString = "";
     
     // Check if SCD30 is ready
     // If yes send CO2 in ppm, Temperature in C and Humidity in % to AIB
@@ -69,6 +90,7 @@ void loop() {
 
     // Measure temperature and humidity
     DHT22_Measure();
+    outsideTemp_Measure();
 
     average_Temperature = (temperature_Measurement_DHT+temperature_Measurement_SCD)/2;
     average_Humidity = (humidity_Measurement_DHT+humidity_Measurement_SCD)/2;
@@ -78,6 +100,7 @@ void loop() {
     dataString += String(average_Humidity) + ';';
     dataString += String(average_Temperature) + ';';
     dataString += String(pressure_Measurement_HP20) + ';';
+    dataString += String(outsideTemperature_Measurement) + ';';
     //dataString += String(CH4_Measurement);
 
     // Send string of sensor data with a new line
@@ -105,4 +128,9 @@ void HP20_Measure()
 void DHT22_Measure(){
     humidity_Measurement_DHT    = dht.readHumidity();
     temperature_Measurement_DHT = dht.readTemperature();
+}
+
+void outsideTemp_Measure(){
+    outsideTempSensor.read();
+    outsideTemperature_Measurement = outsideTempSensor.temperature();
 }
